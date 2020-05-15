b7bad0b8d8a678c7aa700d7520adc524bea89ac6
dzboy82 <45073358+dzboy82@users.noreply.github.com>
2020-05-15 05:48:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`القيصر العظيم الهارب من المدارس السبعة قاهر سكلولو و الكبوب صانع الترند باري `,'https://www.twitch.tv/ayoub_dz_09');
+  client.user.setGame(`القيصر العظيم الهارب من المدارس السبعة قاهر سكلولو و الكبوب صانع الترند باري `,'https://www.twitch.tv/dz27boy');
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
