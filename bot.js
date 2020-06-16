cee5123547cdeda07d52b7d3af920c66a47d4ee0
dzboy82 <45073358+dzboy82@users.noreply.github.com>
2020-06-16 15:14:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`OFF `,'https://www.twitch.tv/dz27boy');
+  client.user.setGame(`!help|القيصر العظيم `,'https://www.twitch.tv/dz27boy');
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
